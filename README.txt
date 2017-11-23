9cdb0168073c74e4cc3a15bbf66e082ba1aec089
Enoch Mbaebie <chukwuemekambaebie@gmail.com>
2017-11-23 21:01:25 +0100

    Fix Footer

---
+++ README.txt
@@ -0,0 +1,9 @@
+Hi there, 
+thanks for visiting our site codewithawa.com.
+To setup this project, just copy this source code
+into your htdocs or www folder (or any folder accessible to your php server)
+and open it on your browser. 
+If you still have a problem opening it, then checkout
+the video on YouTube here: https://www.youtube.com/watch?v=K1CzktZMXhY
+
+Thanks
--- bundle.js
+++ bundle.js
@@ -38,19 +38,46 @@ var img23 = 'logo-nigeria-leadership-aummit.png'
 *******************************************************************************/
 var style = bel`
   <style>
-    body {
+  @import 'http://fonts.googleapis.com/css?family=Alegreya+sans';
+  * {
+     margin: 0;
+      padding: 0;
+      border: 0;
+  }
+  body {
+      background color:#F5F5F5;
+      fonts-family: 'Alegreya', sans-serif;
       margin: 0;
       min-height: 100%;
     }
+  h2 {
+    font-size: 250;
+    font-weight: 700;
+    text-align: center;
+    padding-top: 5% 2% 2% 1%;
+    color: #0000A0;
+  }
+  p {
+    font-size: 150%
+    line-height: 160%;
+    padding: 2%;
+    text-indent: 2%;
+    text-align: justify;
+  }
+  img {
+    max-width: 100;
+    height: auto;
+    width: auto;
+    margin-bottom: -4px;
+  }
     html {
       height: 100%;
+      background color: #0000A0;
     }
     .Menu {
-      list-style-type: none;
       margin: 0;
-      padding: 0;
-      width: 17%;
-      background: rgb(0, 0, 255);
+      padding: 15px 15px 0 0;
+      width: 15%;
       background-image: url(${base + menubackground});
       position: fixed;
       height: 100%;
@@ -58,37 +85,107 @@ var style = bel`
       overflow: auto;
       overflow-x: hidden;
       padding-top: 60px;
-      border: 1px solid #555;
+      border: 0;
     }
+    Anabel-Group-Logo {
+      max-width: 200px;
+      margin: auto;
+      height: 100px;
+      float: right;
+    }
+
     li a {
       display: block;
       color: #000;
       padding: 8px 16px;
       width: 100px;
       text-decoration: none;
-      background-color: font-family: "Open Sans", "Helvetica Neue", Helvetica, sans-serif;
+      float: center;
+      margin-top: 30 px;
+      font-family: "Open Sans", "Helvetica Neue", Helvetica, sans-serif;
       text-align: center;
       border-bottom: 1px solid #555;
     }
-    li a:hover:not(.active) {
-      background-color: #4CAF50;
-      color: red;
-      width: 70%
-    }
-    li a:hover:not(.active) {
+  li a:hover:not(.active) {
       background-color: #a6a8a6;
-      color: red;
-      width: 70%
+      transition:0.6s
+      color: blue;
+      width: 150%
     }
+  a {
+    width: 150 px;
+    color: white;
+    display: block;
+    font-size: 15px;
+    text-align: center;
+    padding:10px;
+    border-radius: 10px;
+    font-family:Century Gothic;
+    font-weight:bold;
 
-  div {
-      list-style-type: none;
+  }
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+    margin: 0;
+    position:absolute;
     }
-    li {
+  ul li {
       display: inline-block;
-      margin: according to the image size;
+      margin: auto;
+      padding: 10px
+      float: center;
+      text-align: right
     }
-  </style>
+  .inner {
+    width: 100;
+    float: none;
+    text-align: left;
+    text font-size: 150%;
+    background-image: url(${base + img16});
+  }
+  .anabelsocial {
+    list-style-type: none;
+    text-align: center;
+    display: inline;
+    font-size: 460%;
+    margin: 5%;
+    padding: 7% 7% 5% 4%;
+
+  }
+
+  .bottom-logo{min-height: 100px;}
+  .bottom-logo img{
+    filter: none;
+    -webkit-transition: all .5s ease;
+    -moz-transition: all .5s ease;
+    -o-transition: all .5s ease;
+    -ms-transition: all .5s ease;
+    transition: all .5s ease;
+    cursor:pointer;
+    opacity: 1.0;
+    margin-top: 50%;
+    margin-left: auto;
+}
+
+.bottom-logo img:hover{
+    filter: grayscale;
+    -webkit-filter: grayscale(1);
+    opacity: 1;
+}
+
+.footer-logos li{
+    list-style: none;
+    display: inline-block;
+    margin: 2% 3.5%;
+    Width: 5%;
+}
+
+.footer-logos li img {
+    Width: 150%;
+}
+</style>
 `
 
 /*******************************************************************************
@@ -96,21 +193,20 @@ var style = bel`
 *******************************************************************************/
 var menu = bel`
   <div class="Menu">
-    <ul>
       <div class="Anabel-Group-Logo">
-        <a href="#"><img src=${base + logo}></a>
-      </div>
-      <li><a href="index.html" class="over">HOME</a></li>
+        <a href="index.html"><img src=${base + logo}></a>
+       <ul>
+      <li><a href="index.html" class="current">HOME</a></li>
+      <li><a href="aboutus.html" >ABOUT US</a></li>
       <li><a href="strategy.html" >STRATEGY</a></li>
-      <li><a href="capital.html">CAPITAL</a></li>
+      <li><a href="capital.html">GLOBAL CAPITAL</a></li>
       <li><a href="leadership.html">LEADERSHIP</a></li>
-      <li><a href="entreprenuers.html">ENTREPRENUERS</a></li>
-      <li><a href="modular.html">ME1000</a></li>
+      <li><a href="entreprenuers.html">ENTREPRENUERSHIP</a></li>
+      <li><a href="modular.html">MASTER CLASS</a></li>
       <li><a href="business.html">BUSINESS EXCELLENCE</a></li>
-    </ul>
-    <div class="clear"></div>
+      </ul>
   </div>
-
+  </div>
 `
 var screen = bel`
   <div class="Screen">
@@ -132,52 +228,42 @@ var screen = bel`
     </div>
 `
 var footer1 = bel`
-<div>
-  <h2 align="left"> Our Mission </h2>
-  <p class="Profile">
-    We are educating Leaders with our leadership academy, engaging future leaders.
-  </p>
-  <h2 align="left"> Our Vision </h2>
-  <p>
-
-To be the most trusted and respected professional services firm recognized by our clients for delivering excellence.</p>
+<div class="inner">
+  <h2> ANABEL GROUP </h2>
+  <h2>Mission: </h2>
+  <p> To provide the Leadership of emerging markets’ private and public sector institutions the strategic support, the tools,
+   the networks and the capital they need<br> to develop local value chains and local capacity to prosper locally and to compete globally.</p>
+  <h2> Vision: </h2>
+  <p>To be the premier global partner for Leaders, Entrepreneurs and Business People seeking to invest, build ecosystems,
+  extend value chains and <br>to build capacity in the emerging markets  </p>
  </div>
-
-
 `
-var footer2 = bel`
-    <div class="wrapper">
-        <ul>
-          <li align="left"><a href="about.html">ABOUT US</a></li>
-        </ul>
-     </div>
-`
+
 var foot1 = bel`
-	<foot>
-	<div class="anabel">
-	  <li>
-	    <img src=${base + img17}>
-	  </li>
-	  <li>
-        <img src=${base + img18}> 
-	  </li>
-	  <li>
-	    <img src=${base + img19}>
-	  </li>
-	  <li>
-	    <img src=${base + img20}>
-	  </li>
-	  <li>
-	    <img src=${base + img21}>
-	  </li>
-	  <li>
-	    <img src=${base + img22}>
-	  </li>
-	  <li>
-	    <img src=${base + img23}>
-	  </li>
-	 </div>
+<section class="bottom-logo">
+    <div class="row">
+         <div class="large-12 columns">
+            <ul class="footer-logos">
+                <li><img src="${base + img17}" alt="" />  </li>
+                <li><img src="${base + img18}" alt="" /></li>
+                <li><img src="${base + img19}" alt="" /> </li>
+                <li><img src="${base + img20}" alt="" /> </li>
+                <li><img src="${base + img21}" alt="" /> </li>
+                <li><img src="${base + img22}" alt="" /></li>
+                <li><img src="${base + img23}" alt="" /></li>
+            </ul>
+        </div> 
+    </div>
+</section>
 
+`
+var footsocial = bel`
+  <div>
+      <ul class="anabelsocial">
+           <li><a href="https://web.facebook.com/anabelgroup" target="_blank"><i class="fa fa-facebook"></i></a></li>
+           <li><a href="https://twitter.com/Anabelleader" target="_blank"><i class="fa fa-twitter"></i></a></li>  
+      </ul>
+  </div>
 `
 /*******************************************************************************
   ASSEMBLE 
@@ -186,8 +272,8 @@ document.head.appendChild(style)
 document.body.appendChild(menu)
 document.body.appendChild(screen)
 document.body.appendChild(footer1)
-document.body.appendChild(footer2)
 document.body.appendChild(foot1)
+/*document.body.appendChild(footsocial)*/
 /*******************************************************************************
   MORE JAVASCRIPT
 *******************************************************************************/
